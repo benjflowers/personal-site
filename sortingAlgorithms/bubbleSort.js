8194c81dc9c15f1403cc3576a9553de6993a1ef4
Ben Flowers <benjaminjflowers@gmail.com>
2020-02-03 22:28:00 -0600

    aligning stacks to bottom of div. Generating new stacks on each click of button - TODO: Implement actual sort

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,7 +1,7 @@
 console.log('begin work to bubble sort data');
 
-const stackCount = 100;
-const stackRange = 100;
+const stackCount = 85;
+const stackRange = 1000;
 const stackArray = Array(stackCount);
 const stacksWrapper = document.querySelector('#stacks');
 const stackButton = document.querySelector('#generateStacks');
@@ -11,18 +11,27 @@ stackButton.addEventListener('click', () => {
 })
 
 function generateStacks() {
+  stacksWrapper.innerHTML = "";
   for(let i = 0; i < stackCount; i++) {
     stackArray[i] = Math.floor(Math.random() * stackRange + 1);
 
     let stackDiv = document.createElement('div');
-    let stackValue = document.createTextNode(`${stackArray[i]}`);
+
+    let stackValue = document.createElement('p');
+    stackValue.innerText = `${stackArray[i]}`;
+    stackValue.setAttribute(
+      "style",
+      'font-size: x-small;'
+    )
 
     stackDiv.setAttribute(
       "style",
       `height: ${stackArray[i]};
        background: gray;
        margin-top: 5px;
-       color: red;`
+       color: red;
+       display: grid;
+       align-items: end;`
     );
 
     stackDiv.appendChild(stackValue);
