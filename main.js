a5dd3ffdf2f26517c5758909ae2d767d15b5f77c
Ben Flowers <benjaminjflowers@gmail.com>
2020-03-27 20:19:12 -0500

    canvas working with clicks

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+let allPosts = getAllEles('post');
+setDates(allPosts);
+
+var canvas  = document.querySelector('canvas#canvasIntro');
+var context = canvas.getContext('2d');
+
+var size = window.innerWidth;
+var dpr = window.devicePixelRation;
+canvas.width = 700//size * dpr;
+canvas.height = 200//size * dpr;
+context.scale(dpr, dpr);
+
+var count = 0;
+canvas.addEventListener('click', e => {
+  let chance = Math.random();
+
+  if(count > 10) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    count = 0;
+  }
+
+  if(chance > .5) {
+    context.moveTo(canvas.width, 0);
+    context.lineTo(0, canvas.height);
+  } else {
+    context.moveTo(0, 0);
+    context.lineTo(canvas.width, canvas.height);
+  }
+  context.lineWidth = random(100);
+  context.strokeStyle = `rgba(${random(200)}, ${random(200)}, ${random(100)}, ${Math.random()})`
+  context.stroke();
+
+  count += 1
+})
+
+//takes two numbers
+//returns random number between two numbers
+function random(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 // takes string argument
 // returns array of all elements
 function getAllEles(element) {
@@ -9,14 +50,14 @@ function getAllEles(element) {
 function setDates(posts) {
   for(let currentPost = 0; currentPost<posts.length; currentPost++) {
     let post = posts[currentPost];
-    console.log(post.children[1].attributes.class.value);
-    let postDateSection = Array.from(post.children).filter( item => item.attributes.class.value == 'date')[0];
+    let postDateSection = filterChildren(post).filter( item => item.attributes.class.value == 'date')[0];
 
     postDateSection.innerText = post.attributes.date.value;
   }
 }
 
-let allPosts = getAllEles('post');
-setDates(allPosts);
-
-
+// takes html element
+// returns all elements with class
+function filterChildren(element) {
+  return Array.from(element.children).filter( item => item.attributes.class != null);
+}
